64732422e80dbe5ba6f3b1db7ff5a6f44903ea97
avanycmurali <avanycmuralidharan@gmail.com>
2026-07-09 22:32:54 +0530

    Student project created and add student created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import AddStudent from './components/AddStudent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <AddStudent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const AddStudent = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Admission Nmuber</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Full Name</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Date of Birth</label>
+<input type="date" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Gender</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Class/Grade</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Section</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Parent Name</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Contact Number</label>
+<input type="tel" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Email Address</label>
+<input type="email" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Address</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<button className="btn btn-success">Submit</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default AddStudent
